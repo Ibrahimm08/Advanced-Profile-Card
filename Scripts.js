31316cb927a7d9a6c1dc4fd657adffba2d31f2ee
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MacBook-Pro.local>
2025-05-08 12:54:31 +0100

    Added back content and profile

--- Scripts.js
+++ Scripts.js
@@ -1,4 +1,42 @@
 function Flip(){
     var card = document.getElementById("card");
     card.classList.toggle("flip");
+
+    var backcontent = getElementById("back-content");
+    backcontent.classList.toggle("flip");
+}
+
+function OpenMenu(){
+    var Btn = document.getElementById("toggle");
+    Btn.classList.toggle("btnActive");
+
+    var backcontent = document.getElementById("back-content");
+    backcontent.classList.toggle("is-open");
+}
+
+function About(){
+    var aboutcontent = document.getElementById("about");
+    var header = document.getElementById("header");
+
+    if(aboutcontent.classList.contains("hidden")){
+        aboutcontent.classList.remove("hidden");
+        header.classList.remove("hidden")
+    }else{
+        aboutcontent.classList.add("hidden");
+        header.classList.add("hidden");
+    }
+}
+
+function Portfolio(){
+    console.log("Portfolio button clicked");
+    var aboutcontent = document.getElementById("about");
+    var header = document.getElementById("header");
+
+    if(aboutcontent.classList.contains("hidden")){
+        aboutcontent.classList.remove("hidden");
+        header.classList.remove("hidden")
+    }else{
+        aboutcontent.classList.add("hidden");
+        header.classList.add("hidden");
+    }
 }
